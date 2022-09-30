441afc0b030a2fa4cffb63436760d3b3e0c83228
Laura Vuorenoja <laura.vuorenoja@gmail.com>
2022-09-30 16:53:36 +0300

    Use arm-compatible version for von-network.

--- services/von-network/wrapper.sh
+++ services/von-network/wrapper.sh
@@ -11,7 +11,8 @@ runVonCommand() {
 	pushd .build > /dev/null
 
 	if [[ ! -d "von-network" ]] ; then
-		git clone https://github.com/bcgov/von-network.git > /dev/null
+		#git clone https://github.com/bcgov/von-network.git > /dev/null
+		git clone https://github.com/lauravuo/von-network.git > /dev/null
 		pushd von-network > /dev/null
 	else
 		pushd von-network > /dev/null
